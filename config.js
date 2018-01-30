2fecd50fc712cd6d43defa2c5ca81631d8ed8107
migaloo <migalooo@yahoo.com>
2018-01-30 17:04:30 +0800

    image load

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   // entry: './src/z-sierpinski/app.js',
   // entry: './src/z-triangle/app.js',
-  entry: './src/z-picture/app.js',
+  // entry: './src/z-picture/app.js',
+  entry: './src/z-mask/app.js',
 }
--- src/z-mask/app.js
+++ src/z-mask/app.js
@@ -5,48 +5,131 @@ import vertexShaderSource from './vertexShader.glsl'
 import fragmentShaderSource from './fragmentShader.glsl'
 import {loadImage} from './util.js'
 
-function render(image) {
-  // Get A WebGL context
-  const gl = createGL('#board')
-  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
-  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
-  // setup GLSL program
-  const program = createProgram(gl, vertexShader, fragmentShader)
+function main() {
+    // Get A WebGL context
+    const gl = createGL('#board')
+    const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
+    const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
+
+    // setup GLSL program
+    const program = createProgram(gl, vertexShader, fragmentShader)
+    gl.useProgram(program)
+    gl.program = program
+
+    // 配置纹理
+    var n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set the positions of the vertices');
+        return;
+    }
+
+    // 清空<canvas>的背景色
+    // gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    if (!initTextures(gl, n)) {
+        console.log('Failed to intialize the texture.');
+        return;
+    }
 }
 
 function initVertexBuffers(gl) {
-  const verticesTexCoords = new Float32Array([
-    -.5, -.5, 0, 1,
-    -.5, -.5, 0, 0,
-     .5, -.5, 1, 1,
-     .5, -.5, 1, 1,
-  ])
-  const n =4
+    var  verticesTexCoords = new Float32Array([
+       -0.5, 0.5, 0.0, 1.0,
+        -0.5, -0.5, 0.0, 0.0,
+        0.5, 0.5, 1.0, 1.0,
+        0.5, -0.5, 1.0, 0.0
+    ]);
+    var n = 4; //点的个数
 
-  const vertexTexCoordBuffer = gl.createBuffer()
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexCoordBuffer)
-  gl.bufferData(gl.array_buffer, verticesTexCoords, gl.STATIC_DRAW)
+    // 创建缓冲区对象
+    var vertexTexCoordBuffer = gl.createBuffer();
+    if (!vertexTexCoordBuffer) {
+        console.log('Failed to create the vertexTexCooredBuffer object ');
+        return -1;
+    }
 
-  const fsize = verticesTexCoords.BYTES_PER_ELEMENT
-  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, fsize*4, 0)
-  gl.enableVertexAttribArray(a_Position)
+    // 将顶点坐标和纹理坐标写入缓冲区对象
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, verticesTexCoords, gl.STATIC_DRAW);
 
-  const a_TexCoord = gl.getAttribLocation(gl.program, 'g_TexCoord')
-  gl.vertexAttribPosinter(a_TexCoord, 2, gl.FLOAT, false, fsize*4, fsize*2)
-  gl.enableVertexAttribArray(a_TexCoord)
+    var FSIZE = verticesTexCoords.BYTES_PER_ELEMENT;
 
-  return n 
+    // 获取a_Position的存储位置,分配缓冲区并开启
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return -1;
+    }
+
+    // 将缓冲区对象分配给a_Position变量
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 4, 0);
+
+    // 连接a_Poisition变量与分配给它的缓冲区对象
+    gl.enableVertexAttribArray(a_Position);
+
+    // 将纹理坐标分配给A_TexCoord并开启它
+
+    var a_TexCoord = gl.getAttribLocation(gl.program, 'a_TexCoord');
+
+    if (a_TexCoord < 0) {
+        console.log('Failed to get the storage location of a_TexCoord');
+        return -1;
+    }
+    gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
+    gl.enableVertexAttribArray(a_TexCoord);// 开启缓冲区分配
+
+    return n
 }
 
-function initTextures(gl, n) {
-  const texture0 = gl.createTexture()
-  const texture1 = gl.createTexture()
+function  initTextures(gl, n) {
+    var texture = gl.createTexture(); // 创建纹理对象
+
+    if (!texture) {
+        console.log('Failed to create the texture object');
+        return false;
+    }
+
+    // 获取u_Sampler的存储位置
+    var u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
+
+    if (!u_Sampler) {
+        console.log('Failed to get the storage location of u_Sampler');
+        return false;
+    }
+
+    const src = './images/sky.jpg'
+    loadImage(src)
+      .then(function(image){
+        loadTexture(gl, n, texture, u_Sampler, image, 1)
+      })
+      .catch(function(err){
+        throw err
+      })
+    
+   return true
 }
 
-loadImage('./images/image.jpg')
-  .then(function(image){
-    render(image)
-  })
-  .catch(function(err){
-    throw err
-  })
+function loadTexture(gl, n , texture, u_Sampler, image, number) {
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); //对纹理图像进行y轴反转
+
+    //开启0号纹理单元
+    gl.activeTexture(gl['TEXTURE'+number]);
+
+    // 向 target 绑定纹理对象
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    // 配置纹理参数
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+    // 配置纹理图像
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+
+    // 将0号纹理传递给着色器
+    gl.uniform1i(u_Sampler, number);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); //绘制矩形
+}
+main()
