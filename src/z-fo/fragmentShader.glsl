e5d83f69daf2f669179d8ea6044e102be0577038
migaloo <migalooo@yahoo.com>
2018-03-14 17:56:59 +0800

    add light normal map

--- src/z-fo/fragmentShader.glsl
+++ src/z-fo/fragmentShader.glsl
@@ -1,8 +1,13 @@
 precision mediump float;
 
+varying vec3 v_normal;
 // Passed in from the vertex shader.
-varying vec4 v_color;
+uniform vec3 u_reverseLightDirection;
+uniform vec4 u_color;
 
 void main() {
-   gl_FragColor = v_color;
+  vec3 normal = normalize(v_normal);
+  float light = dot(normal, u_reverseLightDirection);
+  gl_FragColor = u_color;
+  gl_FragColor.rgb *= light;
 }
--- src/z-fo/vertexShader.glsl
+++ src/z-fo/vertexShader.glsl
@@ -1,15 +1,16 @@
 attribute vec4 a_position;
-attribute vec4 a_color;
+attribute vec3 a_normal;
 
-uniform mat4 u_matrix;
+uniform mat4 u_worldViewProjection;
+uniform mat4 u_world;
 
-varying vec4 v_color;
+varying vec3 v_normal;
 
 void main() {
   // Multiply the position by the matrix.
-  gl_Position = u_matrix * a_position;
+  gl_Position = u_worldViewProjection * a_position;
 
   // Pass the color to the fragment shader.
-  v_color = a_color;
+  v_normal = mat3(u_world) * a_normal;
 }
 
