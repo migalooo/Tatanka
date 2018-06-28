0f1bd2bbd82d45c22deae0bd70d7c7b17652586b
migaloo <migalooo@yahoo.com>
2018-06-28 18:53:16 +0800

    three set uniform

---
+++ src/renders/WebGL.uniformSetter.js
@@ -0,0 +1,266 @@
+import {isEqualArray, copyArray, layoutToArray} from './setUniform.js'
+
+/**
+ * -- Caches --
+ * Float32Array caches used for uploading Matrix uniforms
+ */
+const mat4Float32Array = new Float32Array(16)
+const mat3Float32Array = new Float32Array(9)
+const mat2Float32Array = new Float32Array(4)
+
+function UniformContainer() {
+	this.seq = []
+	this.map = {}
+}
+
+/**
+ * -- Uniform Setters --
+ */
+// Snigle scalar
+function setUniform1f(gl, v) {
+	const cache = this.cache
+	if (cache[0] === v) return
+
+	gl.uniform1f(this.addr, v)
+	cache[0] = v
+}
+
+function setUniform1i(gl, v) {
+	const cache = this.cache
+	if (cache[0] === v) return
+
+	gl.uniform1i(this.addr, v)
+	cache[0] = v
+}
+
+// Single float vector from flat array or object
+function setUniform2fv(gl, v) {
+	const cache = this.cache
+  let values = v
+  if ('x' in v) values = layoutToArray(v, 'xy')
+
+  if (arraysEqual(cache, values)) return 
+
+  gl.uniform2fv(this.addr, values)
+  copyArray(cache, values)
+}
+
+function setUniform3fv(gl, v) {
+	const cache = this.cache
+  let values = v
+  if ('x' in v) values = layoutToArray(v, 'xyz')
+  else if ('r' in v) values = layoutToArray(v, 'rgb')
+
+  if (arraysEqual(cache, values)) return 
+
+  gl.uniform3fv(this.addr, values)
+  copyArray(cache, values)
+}
+
+function setUniform4fv(gl, v) {
+	const cache = this.cache
+  let values = v
+  if ('x' in v) values = layoutToArray(v, 'xyzw')
+
+  if (arraysEqual(cache, values)) return 
+
+  gl.uniform4fv(this.addr, values)
+  copyArray(cache, values)
+}
+
+// Single matrix (from flat array or MatrixN)
+function setUniform2fm(gl, v) {
+	const cache = this.cache
+
+	if ('elements' in v) {
+    const elements = v.elements
+		if (arraysEqual(cache, elements)) return
+
+    mat2Float32Array.set(elements) 
+		gl.uniformMatrix2fv(this.addr, false, mat2Float32Array)
+		copyArray(cache, elements)
+
+	} else {
+		if (arraysEqual(cache, v)) return
+
+		gl.uniformMatrix2fv(this.addr, false, v)
+		copyArray( cache, v)
+	}
+}
+
+function setUniform3fm(gl, v) {
+	const cache = this.cache
+
+	if ('elements' in v) {
+    const elements = v.elements
+		if (arraysEqual(cache, elements)) return
+
+    mat3Float32Array.set(elements) 
+		gl.uniformMatrix3fv(this.addr, false, mat3Float32Array)
+		copyArray(cache, elements)
+
+	} else {
+		if (arraysEqual(cache, v)) return
+
+		gl.uniformMatrix3fv(this.addr, false, v)
+		copyArray( cache, v)
+	}
+}
+
+function setUniform4fm(gl, v) {
+	const cache = this.cache
+
+	if ('elements' in v) {
+    const elements = v.elements
+		if (arraysEqual(cache, elements)) return
+
+    mat4Float32Array.set(elements) 
+		gl.uniformMatrix4fv(this.addr, false, mat4Float32Array)
+		copyArray(cache, elements)
+
+	} else {
+		if (arraysEqual(cache, v)) return
+
+		gl.uniformMatrix4fv(this.addr, false, v)
+		copyArray( cache, v)
+	}
+}
+
+// Single texture (2D/Cube)
+function setUniformTexture2D(gl, v, renderer) {
+	const cache = this.cache
+	const unit = renderer.allocTextureUnit()
+
+	if (cache[0] !== unit) {
+		gl.uniform1i(this.addr, unit)
+		cache[0] = unit
+	}
+
+	renderer.setTexture2D(v || emptyTexture, unit)
+}
+
+function setUniformTextureCube(gl, v, renderer) {
+	const cache = this.cache
+	const unit = renderer.allocTextureUnit()
+
+	if (cache[0] !== unit) {
+		gl.uniform1i(this.addr, unit)
+		cache[0] = unit
+	}
+
+	renderer.setTextureCube(v || emptyCubeTexture, unit)
+}
+
+// Integer / Boolean vectors or arrays thereof (always flat arrays)
+function setUniform2iv( gl, v ) {
+	const cache = this.cache
+	if (arraysEqual(cache, v)) return
+
+	gl.uniform2iv(this.addr, v)
+	copyArray(cache, v)
+}
+
+function setUniform3iv( gl, v ) {
+	const cache = this.cache
+	if (arraysEqual(cache, v)) return
+
+	gl.uniform3iv(this.addr, v)
+	copyArray(cache, v)
+}
+
+function setUniform4iv( gl, v ) {
+	const cache = this.cache
+	if (arraysEqual(cache, v)) return
+
+	gl.uniform4iv(this.addr, v)
+	copyArray(cache, v)
+}
+
+/**
+ * Single unifrom setter
+ */
+function getSingularSetter( type ) {
+	switch ( type ) {
+		case 0x1406: return setUniform1f  // float
+		case 0x8b50: return setUniform2fv // float_vec2
+		case 0x8b51: return setUniform3fv // float_vec3
+		case 0x8b52: return setUniform4fv // float_vec4
+
+		case 0x8b5a: return setUniform2fm // float_mat2
+		case 0x8b5b: return setUniform3fm // float_mat3
+		case 0x8b5c: return setUniform4fm // float_mat4
+
+		case 0x8b5e:
+    case 0x8d66: return setUniformTexture2D    // sampler_2d, sampler_external_oes
+		case 0x8b60: return setUniformTextureCube  // sampler_cube
+
+		case 0x1404:
+    case 0x8b56: return setUniform1i  // int, bool
+		case 0x8b53:
+    case 0x8b57: return setUniform2iv // int_vec2, bool_vec2
+		case 0x8b54:
+    case 0x8b58: return setUniform3iv // int_vec3, bool_vec3
+		case 0x8b55: 
+    case 0x8b59: return setUniform4iv // int_vec4, bool_vec4 
+	}
+}
+
+function SingleUniform( id, activeInfo, addr ) {
+	this.id = id
+	this.addr = addr
+	this.cache = []
+	this.setValue = getSingularSetter(activeInfo.type)
+}
+
+function parseUniform( activeInfo, addr, container ) {
+	const path = activeInfo.name, pathLength = path.length
+
+	RePathPart.lastIndex = 0;
+
+	while ( true ) {
+
+		var match = RePathPart.exec( path ),
+			matchEnd = RePathPart.lastIndex,
+
+			id = match[ 1 ],
+			idIsIndex = match[ 2 ] === ']',
+			subscript = match[ 3 ];
+
+		if ( idIsIndex ) id = id | 0; // convert to integer
+
+		if ( subscript === undefined || subscript === '[' && matchEnd + 2 === pathLength ) {
+
+			// bare name or "pure" bottom-level array "[0]" suffix
+
+			addUniform( container, subscript === undefined ?
+				new SingleUniform( id, activeInfo, addr ) :
+				new PureArrayUniform( id, activeInfo, addr ) );
+
+			break;
+
+		} else {
+			// step into inner node / create it in case it doesn't exist
+
+			var map = container.map, next = map[ id ];
+
+			if ( next === undefined ) {
+				next = new StructuredUniform( id );
+				addUniform( container, next );
+			}
+			container = next;
+		}
+	}
+}
+
+function WebGLUniforms(gl, program) {
+	// UniformContainer.call( this )
+	// this.renderer = renderer
+	const count = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS)
+
+	for (var index=0; index<count; index++) {
+		const activeInfo = gl.getActiveUniform(program, index)
+    const addr = gl.getUniformLocation(program, info.name)
+
+		parseUniform(activeInfo, addr)
+	}
+}
---
+++ src/tools/utils.js
@@ -0,0 +1,46 @@
+function isEqualArray(a, b) {
+  if (a.length !== b.length) return false
+  let i
+	for (i=0, len=a.length; i<len; i++) {
+    if (a[i] !== b[i]) return false
+	}
+  return true
+}
+
+function copyArray(a, b) {
+  let i
+	for (i=0, len=a.length; i<len; i++) {
+    a[i] = b[i]
+	}
+}
+
+function layoutObjectToArray(v, type) {
+  switch (type) {
+    case 'xy':
+      return [v.x, v.y]
+    case 'xyz':
+      return [v.x, v.y, v.z]
+    case 'xyzw':
+      return [v.x, v.y, v.z, v.w]
+    case 'rgb':
+      return [v.r, v.g, v.b]
+  }
+}
+
+function mergeObjectToArray(array, v, offset){
+  if (array === undefined) array = []
+  if (offset === undefined) offset = 0
+
+  array[offset] = v.x
+  array[offset+1] = v.y
+  array[offset+2] = v.z
+  array[offset+3] = v.w 
+  return array
+}
+
+export default {
+  isEqualArray,
+  copyArray,
+  layoutObjectToArray,
+  mergeObjectToArray
+}
