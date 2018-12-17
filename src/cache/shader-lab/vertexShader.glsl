e63c7963903b35294fdd45e1826686d9b822cc82
migaloo <migalooo@yahoo.com>
2018-12-17 09:22:16 +0800

    init three

--- src/cache/shader-lab/vertexShader.glsl
+++
@@ -1,8 +0,0 @@
-attribute vec2 a_position;
-uniform vec2 u_resolution;
-
-void main() {
-  vec2 normal = (a_position/u_resolution) * 2.0 - 1.0;
-  // gl_Position = vec4(normal * vec2(1, -1), 0, 1);
-  gl_Position = vec4(normal , 0, 1);
-}
